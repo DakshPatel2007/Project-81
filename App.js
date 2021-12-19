0a088ae68ffaa7a7a07cee2d35227dac7d25c919
DakshPatel2007 <79900403+DakshPatel2007@users.noreply.github.com>
2021-12-19 18:16:42 +1100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigation from "./navigation/DrawerNavigation";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <DrawerNavigator> 
+    
+    </DrawerNavigator>
+    </NavigationContainer>
+  );
+}
